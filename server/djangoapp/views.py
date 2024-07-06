03bf038b5e62ca10120be903c27539dcc749e75f
Luis Adames <adames1601@gmail.com>
2024-07-06 16:47:44 -0400

    parte 4 realizada

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -98,7 +98,6 @@ def get_dealerships(request, state="All"):
     dealerships = get_request(endpoint)
     return JsonResponse({"status":200,"dealers":dealerships})
 
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
     if(dealer_id):
@@ -120,7 +119,7 @@ def get_dealer_details(request, dealer_id):
         return JsonResponse({"status":200,"dealer":dealership})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
-
+@csrf_exempt
 # Create a `add_review` view to submit a review
 def add_review(request):
     if(request.user.is_anonymous == False):
